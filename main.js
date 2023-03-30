f31db43a7f31e3bc45d4a034a3d71de93f1d1d44
jScriptural <isongjohn014@gmail.com>
2023-03-30 19:36:18 +0100

    updated

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
 //"use strict"
+if(!String.prototype.at){
+	Object.defineProperty(String.prototype,"at",{
+		value(i){
+			let len = this.length;
+			return i < 0? this[len+i]:this[len];
+	},
+		enumerable: false,
+		writable: true,
+		configurable: true
+	});
+	Object.defineProperty(Array.prototype,"at",{
+		value(i){
+			let len = this.length;
+			return i < 0? this[len+i]:this[len]
+		},
+		enumerable:false,
+		writable:true,
+		configurable: true
+	});
+}
 let lowerDisplay = document.querySelector(".exp");
 let upperDisplay = document.querySelector(".result");
 let expression = "";
@@ -19,10 +39,12 @@ document.querySelectorAll(".operators").forEach(_operator=>{
 			let array = Array.from(_operator.dataset.operator);
 		      array.pop();
 		       let checkMultiple = array.join("")
-			if(expression[expression.length-1]!== _operator.dataset.operator && expression[expression.length-1] !== checkMultiple){
-		      
+			if(expression.at(-1) !== _operator.dataset.operator && expression.at(-1) !== checkMultiple){
 			expression += _operator.dataset.operator;
 		toScreen += _operator.dataset.operate;
+			}else if(expression.at(-1) == "(" || expression.at(-1) == ')'){
+				expression += _operator.dataset.operator;
+				toScreen += _operator.dataset.operate;
 			}
 			lowerDisplay.textContent = toScreen;
 		}
@@ -55,11 +77,12 @@ document.querySelector(".cancel").addEventListener("click",()=>{
 	lowerDisplay.textContent =toScreen;
 	upperDisplay.textContent=toScreen;
 });
-document.querySelector(".delete").addEventListener("click",()=>{
+document.querySelector(".delete").addEventListener("click",(evt)=>{
+	 evt.preventDefault();
 	 let popped;
 	 let pruneArray = Array.from(expression);
 	 let truncatedArray = Array.from(toScreen);
-     if(pruneArray[pruneArray.length-1]=="(" &&( pruneArray[pruneArray.length-2] == "s" || pruneArray[pruneArray.length-2] == "n")){
+     if(pruneArray.at(-1) =="(" &&( pruneArray.at(-2) == "s" || pruneArray.at(-2) == "n")){
      	 pruneArray.splice(-4,4);
      	 truncatedArray.splice(-4,4);
      }else {
@@ -80,13 +103,46 @@ document.querySelector(".delete").addEventListener("click",()=>{
 
  
 document.querySelector(".equal").addEventListener("click",()=>{
-	let value = eval(expression);
+	//let value = eval(expression);
+	let f = new Function(`return ${expression}`);
+	let value = f();
 	if(parseInt(value) !== value){
 		 value = +value.toFixed(10);
 	}
 	
 	upperDisplay.textContent =/*`${toScreen} =   */`${value}`;
-	expression = value;
-	toScreen = value;
+	expression = value.toString();
+	toScreen = value.toString();
 	lowerDisplay.textContent="";
 })
+
+function focus(){
+	let prevElem;
+	document.addEventListener("click",evt=>{
+		let currentElem = evt.target;
+		//console.log(currentElem.classList)
+		//if(!currentElem.classList.contains("digits"))return;
+		highlight(currentElem)
+		let timer = setTimeout(()=>{
+			if(currentElem.classList.contains("highlight-for-digits")){
+				currentElem.classList.remove("highlight-for-digits")
+				
+			}else {
+				currentElem.classList.remove("highlight-for-operators")
+			}
+		},400)
+	})
+	function highlight(elem){
+		if(prevElem){
+			prevElem.classList.contains("highlight-for-digits")?prevElem.classList.remove("highlight-for-digits"):prevElem.classList.remove("highlight-for-operators");
+		}
+		if(elem.classList.contains("digits")){
+			elem.classList.add("highlight-for-digits");
+			prevElem = elem;
+		}else if(elem.classList.contains("operators")) {
+			prevElem = elem;
+			elem.classList.add("highlight-for-operators")
+		}
+	}
+}
+focus();
