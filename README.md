79332d09719a1f9adf4feeee0bfd5806ba6873f8
root <root@localhost.localdomain>
2023-06-19 13:30:53 +0000

    update

---
+++ README.md
@@ -0,0 +1,23 @@
+# Calculator 
+
+This is a simple calculator which support trigonometric functions.
+The result of any trigonometric function is in radian by default.
+
+### Weirdness 
+It is mandatory that binary operators be used where they ought to be used.
+For instance, calculations like the one below won't work, though its a common mathematics syntax.
+```
+5Cos(23)
+3sin(30)
+
+```
+The above expression ahould written as 
+```
+5xCos(23)
+3xsin(30)
+```
+respectively.
+---
+
+I had fun. You should too.
+---
--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 //"use strict"
+
+const menuBtn = document.querySelector(".material-icons");
+
 if(!String.prototype.at){
 	Object.defineProperty(String.prototype,"at",{
 		value(i){
@@ -116,6 +119,8 @@ document.querySelector(".equal").addEventListener("click",()=>{
 	lowerDisplay.textContent="";
 })
 
+
+
 function focus(){
 	let prevElem;
 	document.addEventListener("click",evt=>{
@@ -130,7 +135,7 @@ function focus(){
 			}else {
 				currentElem.classList.remove("highlight-for-operators")
 			}
-		},400)
+		},100)
 	})
 	function highlight(elem){
 		if(prevElem){
@@ -146,3 +151,79 @@ function focus(){
 	}
 }
 focus();
+
+
+
+
+
+
+
+function menu(){
+	let menuPage = document.createElement("div");
+	menuPage.className="menu-page";
+	const aboutPage = document.createElement("div");
+	const navBar = document.querySelector("nav");
+	aboutPage.classList.add("about");
+	aboutPage.innerHTML = `
+	<figure>
+	<img src="assets/jscriptural.jpg" width="40%" alt="image of jscriptural">
+	<figcaption>Jscriptural</figcaption>
+	</figure>
+	<div class="biography">
+	<p>
+John Sunday(<b>Jscriptural</b>) is a passionate <b>FrontEnd Developer</b> with comprehensive understanding of modern web development principles and practices. He focuses on creating <b>dynamic and visually appealing user interfaces</b>.
+	</p>
+	</div>
+	<footer>
+	<h1>Let's Connect</h1>
+	<div>
+	<ul>
+				<li><a href="https://github.com/jScriptural" target="_blank" title="connect on github"><i class="fa fa-github-square fa-fw"></i></a></li>
+				<li><a href="https://www.facebook.com/de-mathematical" target="_blank" title="connect on facebook"><i class="fa fa-facebook-square fa-fw"></i></a></li>
+				<li><a href="https://www.twitter.com/kelmathic" target="_blank" title="connect on twitter"><i class="fa fa-twitter-square fa-fw"></i></a></li>
+			</ul>
+	</div>
+	`;
+menuPage.insertAdjacentHTML("afterbegin",`<div class="menuItem">&#10146; About Developer</div>`);
+	menuBtn.addEventListener("click",evt=>{
+		if(menuBtn.textContent == "menu"){
+			navBar.after(menuPage);
+			menuBtn.textContent = `cancel`;
+			menuBtn.style.color = "red";
+		}else {
+			menuPage.style.height= "";
+			setTimeout(()=>menuPage.remove(),101);
+			if(menuPage.querySelector(".about"))menuItem.dispatchEvent(new MouseEvent("click"));
+			menuBtn.textContent = 'menu';
+			menuBtn.style.color = "";
+		}
+
+	});
+
+	let menuItem = menuPage.querySelector(".menuItem");
+	menuItem.addEventListener("click",evt=>{ 
+
+		if(menuPage.querySelector(".about")){
+			setTimeout(()=>aboutPage.remove(),150);
+			menuPage.style.height="";
+		}else {
+			menuItem.after(aboutPage);
+		let aboutHeight = aboutPage.offsetHeight;
+		let height = aboutHeight + menuPage.offsetHeight;
+		console.log(height);
+			menuPage.style.height = height +150+"px";
+				}
+	});
+	/*
+menuPage.ontransitionend = event =>{
+	console.log(event);
+	if(menuPage.querySelector(".about")){
+		aboutPage.remove();
+		return;
+	}
+//	menuItem.after(aboutPage)
+}
+*/
+}
+menu();
+
